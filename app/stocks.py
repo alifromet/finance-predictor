9936f5519281dd314446f5f57146253c4b4d443c
alifromet <fromday26@gmail.com>
2025-06-27 07:49:35 +0300

    Update stocks.py

--- app/stocks.py
+++ app/stocks.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import yfinance as yf
+
+def show():
+    st.header("Stock Market")
+    symbol = st.text_input("Enter Stock Symbol", "AAPL")
+    ticker = yf.Ticker(symbol)
+    hist = ticker.history(period="6mo")
+    st.line_chart(hist["Close"])
 
